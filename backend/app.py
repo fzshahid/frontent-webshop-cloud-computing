53ddaccfc93cc95e41492e58528acc84d6c181d4
fzshahid <linktofaizan@gmail.com>
2024-08-08 17:12:47 +0200

    app.py reverted

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,39 @@
-from flask import Flask, Blueprint
-from flask_cors import CORS
+from flask import Flask
 from extensions import db, mail, login_manager, init_app
 from config import Config
 from flask_migrate import Migrate
 from routes import product_routes, order_routes, payment_routes, inventory_routes, auth_routes
+from flask_cors import CORS
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+  app = Flask(__name__)
+  app.config.from_object(Config)
 
-    # Initialize app extensions
-    init_app(app)
+  # Initialize app extensions
+  init_app(app)
 
-    # Configure CORS to allow specific origins
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
-    app.config['CORS_HEADERS'] = 'Content-Type'
+  # Configure CORS
+  CORS(app, resources={r"/api*": {"origins": "*"}},
+       supports_credentials=True,
+       allow_headers=["Content-Type", "Authorization"],
+       expose_headers=["Content-Type", "Authorization"],
+       methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
-    migrate = Migrate(app, db)
+  migrate = Migrate(app, db)
 
-    with app.app_context():
-        db.create_all()
+  with app.app_context():
+    db.create_all()
 
-    # Register blueprints
-    app.register_blueprint(product_routes.bp)
-    app.register_blueprint(order_routes.bp)
-    app.register_blueprint(payment_routes.bp)
-    app.register_blueprint(inventory_routes.bp)
-    app.register_blueprint(auth_routes.bp)
+  # Register blueprints
+  app.register_blueprint(product_routes.bp)
+  app.register_blueprint(order_routes.bp)
+  app.register_blueprint(payment_routes.bp)
+  app.register_blueprint(inventory_routes.bp)
+  app.register_blueprint(auth_routes.bp)
 
-    return app
+  return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
